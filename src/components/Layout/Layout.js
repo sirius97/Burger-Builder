924b2dab1020ac584f7d04ba73b85e8fd1d79ebc
sirius97 <sirimukund@gmail.com>
2020-04-16 17:32:47 +0530

    burger app basic version complete

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React,{Component} from 'react';
 import classes from './Layout.css';
 import Aux from '../../hoc/Auxi';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-<Aux>
-    <div>Toolbar, SideDrawer, Backdrop</div>
-    <main className = {classes.Content}>{props.children}</main>
-</Aux>
-);
+class Layout extends Component{
+    state = {
+        showSideDrawer: false
+    }
 
-export default layout;
+    sideDrawerCloseHandler = () => {
+        this.setState({showSideDrawer: false})
+    }
+
+    menuClickedHandler = () => {
+        //due to asynchronus behaviour of the set state use the function format
+        this.setState((prevState) => {
+            return {showSideDrawer: !prevState.showSideDrawer};
+        });
+    }
+
+    render(){
+        return (
+            <Aux>
+                <Toolbar clicked = {this.menuClickedHandler}/>
+                <SideDrawer open={this.state.showSideDrawer} close={this.sideDrawerCloseHandler}/>
+                <main className = {classes.Content}>{this.props.children}</main>
+            </Aux>
+        );
+    }
+} 
+export default Layout;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,21 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 import classes from './Modal.css';
 import Aux from '../../../hoc/Auxi';
 import Backdrop from '../Backdrop/Backdrop';
 
 
-const modal = (props) => (
-    <Aux>
-        <Backdrop show = {props.show} backdrop = {props.backclicked} />
-        <div 
-        className = {classes.Modal}
-        style = {{
-            transform : props.show ? 'translateY(0)' : 'translateY(-100vh)',
-            opacity: props.show ? '1' : '0'
-        }}>
-            {props.children}
-        </div>
-    </Aux>
-);
+class Modal extends Component{
 
-export default modal;
+    shouldComponentUpdate(nextProp, nextState){
+        return nextProp.show !== this.props.show 
+    }
+
+    componentDidUpdate(){
+        console.log("Modal updated")
+    }
+
+    render(){
+        return (
+            <Aux>
+            <Backdrop show = {this.props.show} backdrop = {this.props.backclicked} />
+            <div 
+            className = {classes.Modal}
+            style = {{
+                transform : this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                opacity: this.props.show ? '1' : '0'
+            }}>
+                {this.props.children}
+            </div>
+        </Aux>
+        );
+    }
+} 
+
+export default Modal;
